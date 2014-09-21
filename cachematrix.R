1cb765025750f68e3e01c4dd4dbbf77f93c6c6c6
kumar-git <kumar-git@users.noreply.github.com>
2014-09-21 12:37:03 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+#                                               ::: DESCRIPTION :::
+#
+# Function cacheSolve() will take a matrix as an input, it will check whether this matrix and its inverse are 
+# cached or not. If cached then it would return the cached inverse of the matrix. If not then it will cache 
+# the inverse of the matrix. It would also check whether the matrix is same or not to make sure that correct 
+# inverse of matrix would be returned:
+#
+#================================================================================================================
 
-## Write a short comment describing this function
+# Function: makeCacheMatrix will take a matrix as input and cache it:
+makeCacheMatrix <- function(x){
+  cm <<- x
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# Function: cacheSolve will return the inverse of a matrix:  
+cacheSolve <- function(x){
+  if (exists("im") && exists("cm")){
+    imat <- im
+    if (!is.null(imat) && is.matrix(x) && is.matrix(cm) && dim(x) == dim(cm) && all(x == cm)){
+      message("getting cached inverse of matrix")
+      return(imat)
+    } 
+  }
+  makeCacheMatrix(x)
+  im <<- solve(x)
+  im
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
